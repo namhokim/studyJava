2b961783bf60ececdf69eac1c312887b75ec22f4
Namho Kim <namhokim@sindoh.com>
2014-10-31 10:54:06 +0900

    Locale 접미어 확인

---
+++ javaStudySSM/src/main/java/etc/LocaleTest.java
@@ -0,0 +1,14 @@
+package etc;
+
+import java.util.Locale;
+
+public class LocaleTest {
+
+	public static void main(String[] args) {
+		System.out.println(Locale.CHINESE);
+		System.out.println(Locale.KOREAN);
+		System.out.println(Locale.ENGLISH);
+		System.out.println(Locale.US);
+	}
+
+}
