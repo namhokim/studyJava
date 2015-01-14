6bd6fc162a1242bea122591a59da23fb61b576cd
Namho Kim <nhkimnamo@gmail.com>
2015-01-15 00:14:14 +0900

    손 코딩: 에러정정 및 로직정정 1. second 구하기 메서드 로직 오류(손코딩시 아예 없던 메서드) 2. 배열 파라메터로 넘기면 값 수정 안됨 3. Reader.readLine()의 EOF는 null임 4. offset관련 범위 틀림 5. 예외처리(checked exception 처리 필요)

--- javaStudySSM/src/main/java/ch11/no3/NotExistNumberFinder.java
+++ javaStudySSM/src/main/java/ch11/no3/NotExistNumberFinder.java
@@ -1,42 +1,79 @@
 package ch11.no3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class NotExistNumberFinder {
 	
 	public static final int NUMBER_SIZE = 100;
 	public static final int EOF = -1;
 	
-	public int find() {
-		boolean checkFlag[] = new boolean[NUMBER_SIZE - 1];
-		int offset;
+	private boolean checkFlag[] = new boolean[NUMBER_SIZE - 1];
+	private BufferedReader reader;
+	private int offset;
+	
+	public int find() throws IOException {
 		int first, second;
+		if (reader != null) {
+			reader.close();
+		}
+		reader = new BufferedReader(new InputStreamReader(System.in));
 		first = readOneNumber();
 		if (first == EOF) {
-			throw new Exception("Empty data");
+			throw new IllegalArgumentException("Empty data");
 		}
 		second = getNextNumber(first);
 		offset = Math.min(first, second) + 1;
-		setFlag(checkFlag, offset, second);
+		second = Math.max(first, second);
+		setFlag(second);
 		int nextNumber;
-		while ( (nextNumber = readOneNumber) != EOF) {
-			setFlag(checkFlag, offset, nextNumber);
+		while ( (nextNumber = readOneNumber()) != EOF) {
+			setFlag(nextNumber);
 		}
+
 		for (int i=0; i<(NUMBER_SIZE-1); i++) {
 			if (checkFlag[i] == false) {
-				return (i + offset -1);
+				return (i + offset);
 			}
 		}
 		return EOF;
 	}
 
-	private int readOneNumber() {
-		String token = System.in.readLine();
-		return Integer.parseInt(token, 10);
+	private int readOneNumber() throws IOException {
+		String token = reader.readLine();
+		if (token == null) {
+			return EOF;
+		} else {
+			return Integer.parseInt(token, 10);
+		}
 	}
 
-	private void setFlag(boolean[] check, int offset, int value) {
-		int position = value - offset;
-		if (position >=0 && position < NUMBER_SIZE) {
-			check[position] = true;
+	private int getNextNumber(int first) throws IOException {
+		int candidate;
+		while ( (candidate = readOneNumber()) != EOF) {
+			if (candidate != first) {
+				return candidate;
+			}
 		}
+		return first;
+	}
+
+	private void setFlag(int value) {
+		int position = value - offset;
+		if (position >= 0 && position < NUMBER_SIZE) {
+			checkFlag[position] = true;
+		}
+	}
+	
+	/**
+	 * using by stdin redirection
+	 * eg. > java ch11/no3/NotExistNumberFinder < gen.txt
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String args[]) throws IOException {
+		NotExistNumberFinder finder = new NotExistNumberFinder();
+		System.out.println("Not exist number: " + finder.find());
 	}
 }
