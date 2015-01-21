c679b61a7ba2ca63b5b73c868899d9e42feed955
Namho Kim <namhokim@sindoh.com>
2015-01-21 17:01:02 +0900

    new String : case+

--- javaStudySSM/src/main/java/etc/string/StringNew.java
+++ javaStudySSM/src/main/java/etc/string/StringNew.java
@@ -6,11 +6,18 @@ public class StringNew {
 		String str1 = "test";
 		String str2 = "test";
 		String str3 = new String("test");
+		String str4 = "test2";
+		String str5 = new String("test2");
 		
 		System.out.println("1: " + (str1 == str2));
 		System.out.println("2: " + (str1 == str3));
-		System.out.println("3: " + (str1.equals(str2)));
-		System.out.println("4: " + (str1.equals(str3)));
+		System.out.println("3: " + (str1 == str4));
+		System.out.println("4: " + (str1 == str5));
+		System.out.println("5: " + (str1.equals(str2)));
+		System.out.println("6: " + (str1.equals(str3)));
+		System.out.println("7: " + (str1.equals(str4)));
+		System.out.println("8: " + (str1.equals(str5)));
+		
 	}
 	
 }
