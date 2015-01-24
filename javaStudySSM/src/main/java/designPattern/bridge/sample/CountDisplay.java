a845d9d11801df7f010933fbddc25c308c20e346
Namho Kim <nhkimnamo@gmail.com>
2015-01-25 00:31:46 +0900

    Design Pattern + (Adapter, Proxy)

---
+++ javaStudySSM/src/main/java/designPattern/bridge/sample/CountDisplay.java
@@ -0,0 +1,14 @@
+package designPattern.bridge.sample;
+
+public class CountDisplay extends Display {
+    public CountDisplay(DisplayImpl impl) {
+        super(impl);
+    }
+    public void multiDisplay(int times) {       // times회 반복해서 표시하기
+        open();
+        for (int i = 0; i < times; i++) {
+            print();
+        }
+        close();
+    }
+}
