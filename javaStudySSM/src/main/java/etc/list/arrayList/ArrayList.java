0f775d1707880476d17c08ac90fbe050c7c71a87
Namho Kim <namhokim@sindoh.com>
2015-02-16 10:42:05 +0900

    불필요한 주석 삭제

--- javaStudySSM/src/main/java/etc/list/arrayList/ArrayList.java
+++ javaStudySSM/src/main/java/etc/list/arrayList/ArrayList.java
@@ -32,22 +32,18 @@ public class ArrayList<E> implements List<E> {
 	}
 
 	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public <T> T[] toArray(T[] a) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -61,38 +57,30 @@ public class ArrayList<E> implements List<E> {
 	}
 
 	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean addAll(Collection<? extends E> c) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean addAll(int index, Collection<? extends E> c) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public void clear() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public E get(int index) {
@@ -103,42 +91,34 @@ public class ArrayList<E> implements List<E> {
 	}
 
 	public E set(int index, E element) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void add(int index, E element) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public E remove(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public ListIterator<E> listIterator() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public ListIterator<E> listIterator(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public List<E> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
